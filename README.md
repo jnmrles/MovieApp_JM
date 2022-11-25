3a9be7db216209ce14b4ab84946a74f080b0e388
JM <jnmrles@gmail.com>
2022-11-25 15:03:53 -0600

    routes structure

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Filmpire App Jovanni
+
+<!--
+create react app
+Conifigure app.js and add components file
+React.DOM Config in index.js
+set up react router dom routes and switch.
+set up your route path layout
+-->
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./components/App.jsx";
 
 // all components in App.jsx and this will get rendered on root html
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  // eslint-disable-next-line comma-dangle
+  document.getElementById("root")
+);
