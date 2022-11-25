1e3140552c16259577f7e88c4586027db06933ca
JM <jnmrles@gmail.com>
2022-11-25 13:50:33 -0600

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App.jsx";
+
+// all components in App.jsx and this will get rendered on root html
+ReactDOM.render(<App />, document.getElementById("root"));
